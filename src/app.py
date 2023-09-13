7fdecb578b9219224dc32f6437f628897e0f1d4d
DeeeTeeee <david.anifowoshe@yandex.com>
2023-09-13 09:30:37 +0000

    init

--- src/app.py
+++ src/app.py
@@ -1,256 +1,142 @@
-# #importing the libraries;
-# from fastapi import FastAPI
-# from uvicorn import run as uvicorn_run
-# from typing import List, Literal
-# from pydantic import BaseModel
-# import joblib
-# from fastapi.encoders import jsonable_encoder
-# import uvicorn
-
-
-# #CONFIG
-# app = FastAPI(
-#     title="Income Prediction API",
-#     version="0.0.1",
-#     description="This is a simple API for Income prediction")
-
-# #API INPUT
-# class Input(BaseModel):
-#     age: int 
-#     gender: object 
-#     education: object 
-#     marital_status: object 
-#     race: object 
-#     employment_stat: int
-#     wage_per_hour: int
-#     working_week_per_year: int  
-#     industry_code: int
-#     occupation_code: int  
-#     total_employed: int 
-#     vet_benefit: int  
-#     tax_status: object 
-#     gains: int 
-#     losses: int  
-#     stocks_status: int 
-#     citizenship: object 
-#     mig_year: int  
-#     importance_of_record: float
-#     #income_above_limit
-    
-    
-#     #ENDPOINT
-#     @app.get("/")
-#     async def root():
-#         return {"message": "Online"}
-    
-#     # @app.post("/predict")
-#     # def predict(input: Input):
-#     #     scaler = joblib.load("Assets/scaler.joblib")
-#     #     model = joblib.load("Assets/model.joblib")
-        
-#     #     features = [input.age,    
-#     # input.gender,                 
-#     # input.education,                 
-#     # input.marital_status,               
-#     # input.race,                     
-#     # input.employment_stat,
-#     # input.wage_per_hour,
-#     # input.working_week_per_year,
-#     # input.industry_code,
-#     # input.occupation_code,
-#     # input.total_employed,
-#     # input.vet_benefit,
-#     # input.tax_status,
-#     # input.gains,
-#     # input.losses,
-#     # input.stocks_status,
-#     # input.citizenship,
-#     # input.mig_year,
-#     # input.importance_of_record]
-    
-    
-#     # scale_features = scaler.transform([features])[0]
-#     # prediction = model.predict([scale_features])[0]
-    
-#     # #Serializing the prediction using jsonable_encoder
-#     # serialized_prediction = jsonable_encoder({"prediction": int(prediction)})
-#     # if serialized_prediction == 1:
-#     #     result=  {"prediction": "Income above limit"}
-#     # else:
-#     #     result= {"prediction": "Income below limit"}
-#     #     return result
-        
-# if __name__ == "__main__":
-#     uvicorn.run('app:app', reload =True)
-#         #uvicorn.run(app, host="000000000", port=8000)
-        
-
-# # Importing the libraries
-# from fastapi import FastAPI
-# from uvicorn import run as uvicorn_run
-# from typing import List, Literal
-# from pydantic import BaseModel
-# import joblib
-# from fastapi.encoders import jsonable_encoder
-# import uvicorn
-
-# # CONFIG
-# app = FastAPI(
-#     title="Income Prediction API",
-#     version="0.0.1",
-#     description="This is a simple API for Income prediction")
-
-# # API INPUT
-# class Input(BaseModel):
-#     age: int 
-#     gender: object 
-#     education: object 
-#     marital_status: object 
-#     race: object 
-#     employment_stat: int
-#     wage_per_hour: int
-#     working_week_per_year: int  
-#     industry_code: int
-#     occupation_code: int  
-#     total_employed: int 
-#     vet_benefit: int  
-#     tax_status: object 
-#     gains: int 
-#     losses: int  
-#     stocks_status: int 
-#     citizenship: object 
-#     mig_year: int  
-#     importance_of_record: float
-
-# # ENDPOINT
-# @app.get("/")
-# async def root():
-#     return {"message": "Online"}
-
-# @app.post("/predict")
-# def predict(input: Input):
-#     encoder = joblib.load("Assets/encoder.joblib")
-#     scaler = joblib.load("Assets/scaler.joblib")
-#     model = joblib.load("Assets/model.joblib")
-        
-#     features = [input.age,    
-#     input.gender,                 
-#     input.education,                 
-#     input.marital_status,               
-#     input.race,                     
-#     input.employment_stat,
-#     input.wage_per_hour,
-#     input.working_week_per_year,
-#     input.industry_code,
-#     input.occupation_code,
-#     input.total_employed,
-#     input.vet_benefit,
-#     input.tax_status,
-#     input.gains,
-#     input.losses,
-#     input.stocks_status,
-#     input.citizenship,
-#     input.mig_year,
-#     input.importance_of_record]
-    
-#     encode_features = encoder.transform([features])[0]
-#     scale_features = scaler.transform([encode_features])[0]
-#     prediction = model.predict([scale_features])[0]
-    
-#     # Serializing the prediction using jsonable_encoder
-#     serialized_prediction = jsonable_encoder({"prediction": int(prediction)})
-#     if serialized_prediction == 1:
-#         result = {"prediction": "Income above limit"}
-#     else:
-#         result = {"prediction": "Income below limit"}
-#     return result
-
-# if __name__ == "__main__":
-#     uvicorn.run('app:app', reload=True)
-
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+#Importing the libraries
+import gradio as gr
+import pickle
+import pandas as pd
+import numpy as np
 import joblib
-import uvicorn
+from PIL import Image
 
-# Create FastAPI app
-app = FastAPI(
-    title="Income Prediction API",
-    version="0.0.1",
-    description="This is a simple API for Income prediction"
-)
+#using joblib to load the model:
+# num_imputer = joblib.load('num_imputer.joblib') # loading the imputer 
+# cat_imputer = joblib.load('cat_imputer.joblib') # loading the imputer
+encoder = joblib.load('encoder.joblib') # loading the encoder
+scaler = joblib.load('scaler.joblib') # loading the scaler
+model = joblib.load('ml.joblib') # loading the model
 
-# Define input model
-class Input(BaseModel):
-    age: int
-    gender: str  # Change 'object' to 'str' if gender is a string
-    education: str
-    marital_status: str
-    race: str
-    employment_stat: int
-    wage_per_hour: int
-    working_week_per_year: int
-    industry_code: int
-    occupation_code: int
-    total_employed: int
-    vet_benefit: int
-    tax_status: str
-    gains: int
-    losses: int
-    stocks_status: int
-    citizenship: str
-    mig_year: int
-    importance_of_record: float
 
-# Define prediction endpoint
-@app.post("/predict")
-def predict(input: Input):
-    # Load your model and encoders
-    encoder = joblib.load("Assets/encoder.joblib")
-    scaler = joblib.load("Assets/scaler.joblib")
-    model = joblib.load("Assets/model.joblib")
+# Create a function that applies the ML pipeline and makes predictions
+def predict(age,gender,education,marital_status,race,employment_stat,wage_per_hour,working_week_per_year,industry_code,occupation_code,
+    total_employed,vet_benefit,tax_status,gains,losses,stocks_status,citizenship,mig_year,importance_of_record):
+
+
+
+    # Create a dataframe with the input data
+     input_df = pd.DataFrame({
+        'age': [age],
+        'gender': [gender],
+        'education': [education],
+        'marital_status': [marital_status],
+        'race': [race],
+        'employment_stat': [employment_stat],
+        'wage_per_hour': [wage_per_hour],
+        'working_week_per_year': [working_week_per_year],
+        'industry_code': [industry_code],
+        'occupation_code': [occupation_code],
+        'total_employed': [total_employed],
+        'vet_benefit': [vet_benefit],
+        'tax_status': [tax_status],
+        'gains': [gains],
+        'losses': [losses],
+        'stocks_status': [stocks_status],
+        'citizenship': [citizenship],
+        'mig_year': [mig_year],
+        'importance_of_record': [importance_of_record]
+
+ })
+
+# Create a list with the categorical and numerical columns
+     cat_columns = [col for col in input_df.columns if input_df[col].dtype == 'object']
+     num_columns = [col for col in input_df.columns if input_df[col].dtype != 'object']
+
+    # # Impute the missing values
+    #  input_df_imputed_cat = cat_imputer.transform(input_df[cat_columns]) 
+    #  input_df_imputed_num = num_imputer.transform(input_df[num_columns]) 
+
+    # Encode the categorical columns
+     input_encoded_df = pd.DataFrame(encoder.transform(input_df[cat_columns]).toarray(),
+                                   columns=encoder.get_feature_names_out(cat_columns))
+
+    # Scale the numerical columns
+     input_df_scaled = scaler.transform(input_encoded_df)
+     input_scaled_df = pd.DataFrame(input_df_scaled , columns = num_columns)
+
+
+    #joining the cat encoded and num scaled
+     final_df = pd.concat([input_encoded_df, input_scaled_df], axis=1)
+
+    #  final_df = final_df.reindex(columns=['SeniorCitizen','tenure','MonthlyCharges','TotalCharges',
+    #  'gender_Female','gender_Male','Partner_No','Partner_Yes','Dependents_No','Dependents_Yes','PhoneService_No',
+    #  'PhoneService_Yes','MultipleLines_No','MultipleLines_Yes','InternetService_DSL','InternetService_Fiber optic',
+    #  'InternetService_No','OnlineSecurity_No','OnlineSecurity_Yes','OnlineBackup_No','OnlineBackup_Yes','DeviceProtection_No',
+    #  'DeviceProtection_Yes','TechSupport_No','TechSupport_Yes','StreamingTV_No','StreamingTV_Yes','StreamingMovies_No',
+    #  'StreamingMovies_Yes','Contract_Month-to-month','Contract_One year','Contract_Two year','PaperlessBilling_No',
+    #  'PaperlessBilling_Yes','PaymentMethod_Bank transfer (automatic)','PaymentMethod_Credit card (automatic)','PaymentMethod_Electronic check',
+    #  'PaymentMethod_Mailed check'])
+
+    # Make predictions using the model
+     predict = model.predict(final_df)
+
+
+     prediction_label = "INCOME ABOVE LIMIT" if predict.item() == '1' else "INCOME BELOW LIMIT"
+
+
+     return prediction_label
+
+     #return predictions
+
+#define the input interface
+
+
+input_interface = []
+
+with gr.Blocks(css=".gradio-container {background-color:silver}") as app:
+    title = gr.Label('INCOME PREDICTION APP.')
+    img = gr.Image("income_image.png").style(height= 210 , width= 1250)
+
+ 
+    with gr.Row():
+        gr.Markdown("This application provides predictions on whether a customer will churn or remain with the Company. Please enter the customer's information below and click PREDICT to view the prediction outcome.")
+
+    with gr.Row():
+        with gr.Column(scale=3.5, min_width=500):
+            input_interface = [
+                gr.components.Number(label="How Old are you?")
+                gr.components.Radio(['male', 'female'], label='What is your Gender?'),
+                gr.components.Dropdown(['High School', 'left', 'Undergrad', 'Grad', 'Associate Degree',
+                                         'Doctorate'], label='What is your level of education?'),
+                gr.components.Dropdown(['Widowed', 'Single', 'Married', 'Divorced', 'Separated'], label='Marital Status?'),
+                gr.components.Dropdown([' White', ' Black', ' Asian or Pacific Islander',
+                                        ' Amer Indian Aleut or Eskimo', ' Other'], label='Whats your race?'),
+                gr.components.Dropdown([0, 2, 1], label='Whats your emploment status? (0 = Unemployed, 1 = Self-Employed, 2 = Employed)'),
+                gr.components.Number(label='How much is your Wage per Hour? (0 - 10000)'),
+                gr.components.Number(label='How many weeks have you worked in a year? (1 - 52)'),
+                gr.components.Number(label='How many working weeks per year do you work?'),
+                gr.components.Number(label='What is your Industry Code? (1 - 51)'),
+                gr.components.Number(label='What is your occupation Code? (1 - 46)'),
+                gr.components.Number(label='Number of persons working for employer? (1 - 7)')
+                gr.components.Number(label='Benefit? (1 - 3)') 
+                gr.components.Dropdown([' Head of household', ' Single', ' Nonfiler', ' Joint both 65+',
+                                        ' Joint one 65+ & one under 65', ' Joint one under 65 & one 65+'],label='Whats your tax status?')
+                gr.components.Number(label='What is your Gain'), 
+                gr.components.Number(label='What is your Loss'),
+                gr.components.Number(label='What is your Stock Status'),
+                gr.components.Dropdown(['Native', ' Foreign born- Not a citizen of U S ',
+                                         ' Foreign born- U S citizen by naturalization',
+                                         ' Native- Born abroad of American Parent(s)',
+                                         ' Native- Born in U S',' Native- Born in Puerto Rico or U S Outlying'], label='Whats is your Citizenshiip?'),
+                gr.components.Radio([94,95], label='Whats your year of migration?')
+                gr.components.Number(label='Whats your Weight Of Instance?')           
+                ]
+
+    with gr.Row():
+        predict_btn = gr.Button('Predict') 
         
-    # Prepare input features
-    features = [input.age,    
-                input.gender,                 
-                input.education,                 
-                input.marital_status,               
-                input.race,                     
-                input.employment_stat,
-                input.wage_per_hour,
-                input.working_week_per_year,
-                input.industry_code,
-                input.occupation_code,
-                input.total_employed,
-                input.vet_benefit,
-                input.tax_status,
-                input.gains,
-                input.losses,
-                input.stocks_status,
-                input.citizenship,
-                input.mig_year,
-                input.importance_of_record]
-    
-    # Encode and scale features
-    encode_features = encoder.transform([features])[0]
-    scale_features = scaler.transform([encode_features])[0]
-    
-    # Make a prediction
-    prediction = model.predict([scale_features])[0]
-    
-    # Determine the prediction result
-    if prediction == 1:
-        result = {"prediction": "Income above limit"}
-    else:
-        result = {"prediction": "Income below limit"}
-    
-    return result
+ 
 
-# Define root endpoint
-@app.get("/")
-async def root():
-    return {"message": "Online"}
+# Define the output interfaces
+    output_interface = gr.Label(label="INCOME ABOVE LIMIT")
 
-if __name__ == "__main__":
-    uvicorn.run('app:app', reload=True)
+    predict_btn.click(fn=predict, inputs=input_interface, outputs=output_interface)
+
+
+    app.launch(share=True)
